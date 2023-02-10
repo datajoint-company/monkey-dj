51b7b849ae0bc18268bff8b164982653bdc1f590
JaerongA <jaerong.ahn@datajoint.com>
2023-02-10 15:15:39 -0600

    initialize schema

--- init.m
+++ init.m
@@ -1,25 +1,28 @@
-clear all
+% clear all
 
 % Load configuration
 dj.config();
 dj.config.load('.\dj_local_conf.json')
 
 global databasePrefix
-databasePrefix = 'causality-in-motion_angelaki-lab_';
+databasePrefix = dj.config().custom.databasePrefix
 
 schema_names = {'firefly'};
 
+% Bring the schema name into the namespace as a struct format 
+% Tables defined under the schema can be acessed with e.g.,
+% dj_{schema_name}.{table_name} (dj_firefly.Session)
 for k = 1: numel(schema_names)
-clear schema
-schema_name = schema_names{k};
-eval(['import ', schema_name, '.getSchema']);
-eval(['schema = ', schema_name, '.getSchema();'])
-
-for j = 1:numel(schema.classNames)
-    tbl_name = split(schema.classNames{j}, '.');
-    tbl_name = join(tbl_name{2:end}, '.');
-    eval(['v_', schema_name, '.(tbl_name) = schema.v.(tbl_name);']);
-end 
-
-eval(['v_', schema_name, '.schema = schema;'])
+    clear schema
+    schema_name = schema_names{k};
+    eval(['import ', schema_name, '.getSchema']);
+    eval(['schema = ', schema_name, '.getSchema();'])
+    
+    for j = 1:numel(schema.classNames)
+        tbl_name = split(schema.classNames{j}, '.');
+        tbl_name = join(tbl_name{2:end}, '.');
+        eval(['dj_', schema_name, '.(tbl_name) = schema.v.(tbl_name);']);
+    end 
+    
+    eval(['dj_', schema_name, '.schema = schema;'])
 end
