f5d47874cd10856d519be20030b61196c9889b31
JaerongA <jaerong.ahn@datajoint.com>
2023-02-10 11:49:40 -0600

    add a template dj_config and initialization script

---
+++ init.m
@@ -0,0 +1,25 @@
+clear all
+
+% Load configuration
+dj.config();
+dj.config.load('.\dj_local_conf.json')
+
+global databasePrefix
+databasePrefix = 'causality-in-motion_angelaki-lab_';
+
+schema_names = {'firefly'};
+
+for k = 1: numel(schema_names)
+clear schema
+schema_name = schema_names{k};
+eval(['import ', schema_name, '.getSchema']);
+eval(['schema = ', schema_name, '.getSchema();'])
+
+for j = 1:numel(schema.classNames)
+    tbl_name = split(schema.classNames{j}, '.');
+    tbl_name = join(tbl_name{2:end}, '.');
+    eval(['v_', schema_name, '.(tbl_name) = schema.v.(tbl_name);']);
+end 
+
+eval(['v_', schema_name, '.schema = schema;'])
+end
