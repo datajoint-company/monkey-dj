84352573f2854dcadc0cbf039335e265e0a7d7b4
JaerongA <jaerong.ahn@datajoint.com>
2023-02-12 14:01:04 -0600

    move the previous version to +firefly_prev

--- +firefly_prev/getSchema.m
+++ +firefly_prev/getSchema.m
@@ -2,7 +2,7 @@ function obj = getSchema
 persistent schemaObject
 if isempty(schemaObject)
     global databasePrefix
-    schemaObject = dj.Schema(dj.conn, 'firefly', [databasePrefix, 'firefly']);
+    schemaObject = dj.Schema(dj.conn, 'firefly_prev', [databasePrefix, 'firefly_prev']);
 end
 obj = schemaObject;
 end
